24bea622368751959d1ba54685c77db3cef06f54
REYES_JOHN RUIZ P. <reyesjohn597@gmail.com>
2025-04-11 05:39:16 +0800

    fix student sidebar and some main content margin.

--- student/ReservationHistory.js
+++ student/ReservationHistory.js
@@ -67,3 +67,20 @@ function formatDateTime(dateTimeStr) {
         hour12: true 
     });
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebar = document.querySelector('.sidebar');
+    const burgerMenu = document.querySelector('.burger-menu');
+    const facilities = document.querySelectorAll('.facility img'); // Select all facility images
+
+    function toggleSidebar() {
+        sidebar.classList.toggle('collapsed');
+    }
+
+    if (burgerMenu) {
+        burgerMenu.addEventListener('click', toggleSidebar);
+    } else {
+        console.error("Burger menu button not found!");
+    }
+
+   
+        });
