84ed9aa0bf478684c18a67969ec8a9044b2150ae
bergado-shamma <shabergado.22@gmail.com>
2025-06-06 10:35:05 +0800

    Upload our updated log-in and sign-up module with emailing features

---
+++ pocketbase/pb_migrations/1749128135_updated_users.js
@@ -0,0 +1,108 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update field
+  collection.fields.addAt(9, new Field({
+    "hidden": false,
+    "id": "select3655317636",
+    "maxSelect": 1,
+    "name": "organization",
+    "presentable": true,
+    "required": false,
+    "system": false,
+    "type": "select",
+    "values": [
+      "CSC",
+      "AECES",
+      "CS",
+      "JMA",
+      "JPIA",
+      "JPMAP",
+      "JPSME",
+      "PASOA",
+      "MS",
+      "PUPUKAW",
+      "ERG"
+    ]
+  }))
+
+  // update field
+  collection.fields.addAt(10, new Field({
+    "hidden": false,
+    "id": "select288516751",
+    "maxSelect": 1,
+    "name": "course",
+    "presentable": true,
+    "required": false,
+    "system": false,
+    "type": "select",
+    "values": [
+      "BSECE",
+      "BSME",
+      "BSBAHRM",
+      "BSBAMM",
+      "BSIT",
+      "BSED-MATH",
+      "BSED-ENGLISH",
+      "BSOA",
+      "DIT",
+      "DOMT"
+    ]
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update field
+  collection.fields.addAt(9, new Field({
+    "hidden": false,
+    "id": "select3655317636",
+    "maxSelect": 1,
+    "name": "organization",
+    "presentable": true,
+    "required": true,
+    "system": false,
+    "type": "select",
+    "values": [
+      "CSC",
+      "AECES",
+      "CS",
+      "JMA",
+      "JPIA",
+      "JPMAP",
+      "JPSME",
+      "PASOA",
+      "MS",
+      "PUPUKAW",
+      "ERG"
+    ]
+  }))
+
+  // update field
+  collection.fields.addAt(10, new Field({
+    "hidden": false,
+    "id": "select288516751",
+    "maxSelect": 1,
+    "name": "course",
+    "presentable": true,
+    "required": true,
+    "system": false,
+    "type": "select",
+    "values": [
+      "BSECE",
+      "BSME",
+      "BSBAHRM",
+      "BSBAMM",
+      "BSIT",
+      "BSED-MATH",
+      "BSED-ENGLISH",
+      "BSOA",
+      "DIT",
+      "DOMT"
+    ]
+  }))
+
+  return app.save(collection)
+})
